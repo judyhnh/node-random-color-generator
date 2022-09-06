486a217cb76c2c8fe65f7b68d96b14caad985b70
judyhnh <judyhnh@gmail.com>
2022-09-06 22:48:12 +0200

    Update color generator

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log('Hello');
+import { argv } from 'node:process';
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+let color = randomColor();
+
+const box = `
+###############################
+###############################
+###############################
+#####                     #####
+#####        ${color}      #####
+#####                     #####
+###############################
+###############################
+###############################`;
+
+if (argv[2] && argv[3] === 'light') {
+  color = randomColor({
+    hue: argv[2],
+    luminosity: 'light',
+  });
+  console.log(chalk.hex(`${color}`)(`${box}`));
+} else if (argv[2] && argv[3] === 'dark') {
+  color = randomColor({
+    hue: argv[2],
+    luminosity: 'dark',
+  });
+  console.log(chalk.hex(`${color}`)(`${box}`));
+} else if (argv[2]) {
+  color = randomColor({
+    hue: argv[2],
+  });
+  console.log(chalk.hex(`${color}`)(`${box}`));
+} else {
+  console.log(chalk.hex(`${color}`)(`${box}`));
+}
